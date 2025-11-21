a09f6f38c35fda2e4e83900a43d551c879ca4a42
Jay Mills <jay_michael_mills@live.com>
2025-11-20 20:35:21 -0500

    Cleaned buggy platforms again

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,25 +26,15 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // if (isGrounded)
-        // {
-        //     Debug.Log("GROUNDED");
-        // }
-        // if (!isGrounded)
-        // {
-        //     Debug.Log("NOT GROUNDED");
-        // }
-        
         // --- Horizontal movement ---
         // Get input from keyboard (A/D or Left/Right arrows).
         float moveInput = Input.GetAxis("Horizontal");
         // Apply horizontal speed while keeping the current vertical velocity.
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-        //Debug.Log("TIMER DONE");
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
-            //Debug.Log("JUMPED");
 
             timer = 0f;
         }
