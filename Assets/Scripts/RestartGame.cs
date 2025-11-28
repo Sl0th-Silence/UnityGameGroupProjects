79a90af84df41a6487a7cf07c37508fe1fd77eee
Caleb <caleb.ohara@student.sl.on.ca>
2025-11-28 16:35:15 -0500

    Added win condition

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class RestartGame : MonoBehaviour
+{
+    public void LoadCurrentScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+        Time.timeScale = 1;
+
+    }
+}
